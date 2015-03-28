0feec67d0a66fd58543f86803216678b716b2df3
James Futhey <futhey@gmail.com>
2015-03-28 09:59:54 -0700

    First

---
+++ core.js
@@ -0,0 +1,146 @@
+/****************************************************
+ * Core.js
+ ***************************************************/
+
+/**
+ * a Generic core object
+ */
+var Core = function () {
+
+    /**
+     * datastore getter
+     */
+    function get (key) {
+        return this.data[key];
+    }
+
+
+    /**
+     * datastore setter
+     */
+    function set (key, value) {
+        return this.data[key] = value;
+    }
+
+
+    /**
+     * Executes an array of functions, Sequentially
+     */
+    function executeFunctionArray (functionArray, args) {
+        if (typeof(functionArray) !== "object" || !functionArray.length) return false;
+
+        for (var i = 0; i < functionArray.length; i++) {
+            args = functionArray[i](args);
+        }
+
+        return args;
+    }
+
+
+    /**
+     * Registers a new global on the current object
+     */
+    function registerGlobal (key, value) {
+
+        if (typeof(this[key]) === "undefined") {
+
+            if (typeof(value) === "function") {
+
+                this[key] = function () {
+                    /**
+                     * Prepare Arguments
+                     *
+                     * TODO: (Source: MDN)
+                     * You should not slice on arguments because it prevents optimizations in JavaScript
+                     * engines (V8 for example). Instead, try constructing a new array by iterating
+                     * through the arguments object.
+                     */
+                    // var args = Array.prototype.slice.call(arguments);
+                    var args = arguments;
+                    if (args.length === 0) args = null;
+
+                    /**
+                     * Execute Before hooks on the arguments
+                     */
+                    if (this.hooks[key] && this.hooks[key].before && this.hooks[key].before.length > 0)
+                        args = executeFunctionArray(this.hooks[key].before, args);
+
+                    /**
+                     * Execute the intended function
+                     */
+                    result = value.apply(this, args);
+
+                    /**
+                     * Execute After hooks on the result
+                     */
+                    if (this.hooks[key] && this.hooks[key].after && this.hooks[key].after.length > 0)
+                        result = executeFunctionArray(this.hooks[key].after, result);
+
+                    return result;
+                };
+
+            } else {
+
+                // If the global is being set to any other type of object or value, just do it.
+                this[key] = value;
+
+            }
+
+        } else {
+            console.log("ERROR: A module attempted to write to the `" + key + "` namespace, but it is already being used.");
+        }
+    }
+
+
+    /**
+     * Registers a new before hook on a method
+     *
+     * Example:
+     * We could add a before hook to generateUID which always set the separator to `+`
+     *
+     * ```javascript
+     * this.before('generateUID', function(args) {
+     *     if (args) args[0] = '+';
+     *     return args;
+     * });
+     * ```
+     *
+     * Then, when we called generateUID('-'), we would get a GUID separated by `+` instead.
+     *
+     * TODO: Consider moving this.before & this.after to a private namespace to they cannot
+     * be easily accessed by 3rd party code.
+     *
+     */
+    function before (key, func) {
+        if (!this.hooks[key]) this.hooks[key] = {};
+        if (!this.hooks[key].before) this.hooks[key].before = [];
+        this.hooks[key].before.push(func);
+    }
+
+
+    /**
+     * Registers a new after hook on a this method
+     */
+    function after (key, func) {
+        if (!this.hooks[key]) this.hooks[key] = {};
+        if (!this.hooks[key].after) this.hooks[key].after = [];
+        this.hooks[key].after.push(func);
+    }
+
+
+    /**
+     * Return public objects & methods
+     */
+    obj = {
+        data: {},
+        hooks: {},
+        executeFunctionArray: executeFunctionArray,
+        registerGlobal: registerGlobal,
+        before: before,
+        after: after,
+        get: get,
+        set: set
+    };
+
+    return obj;
+};
